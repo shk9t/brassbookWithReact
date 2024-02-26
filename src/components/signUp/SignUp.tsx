1a66b22f3ada5ae66367a2d7b6c6429911662ed8
shk9t <147887719+shk9t@users.noreply.github.com>
2024-02-26 18:30:21 +0300

    deleted:    dist/assets/child1-asSFs2jE.png 	deleted:    dist/assets/icon1-CF6iXRly.svg 	deleted:    dist/assets/icon2-wqGGR4yC.svg 	deleted:    dist/assets/index-7G7KKVsH.css 	deleted:    dist/assets/index-VhC2-8AE.js 	deleted:    dist/assets/notes-6NQXfUyh.png 	deleted:    dist/assets/notesFromTube-Gwh8Ovhk.png 	deleted:    dist/assets/parner-v_LJWmX4.png 	deleted:    dist/assets/piano-za3JhM53.png 	deleted:    dist/assets/stars-PxZ7AGsM.svg 	deleted:    dist/assets/tube-Di6wWKt5.png 	deleted:    dist/assets/tube2-aOjD37RU.png 	deleted:    dist/assets/tutor1-Smk1QmhG.png 	deleted:    dist/assets/tutor2-HxVIQgvO.png 	deleted:    dist/assets/videoprev1-MWlBXwdj.png 	deleted:    dist/index.html 	modified:   src/components/signUp/SignUp.tsx

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -6,21 +6,24 @@ import { useState } from "react";
 
 function SignUp() {
 
+    const [registrationType, setRegistrationType] = useState('personal')
+
 
     return (
         <div className="sign-in">
             <div className="sign-in__container container">
                 <div className="sign-in__info">
                     <NavLink to={'/'} className="sign-in__backlink"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M20 12H4M4 12L10 18M4 12L10 6" stroke="#190636" stroke-opacity="0.3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M20 12H4M4 12L10 18M4 12L10 6" stroke="#190636" strokeOpacity="0.3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>Вернуться на главную</NavLink>
                     <h1 className={"sign-in__title"}>Регистрация</h1>
                     <p className="sign-in__text">Выберите тип регистрации и введите необходимые данные. <br/>
                         Вы уже зарегистрированны на сайте? <NavLink to='/signin'>Войти</NavLink>
                     </p>
                 </div>
-                <SignUpForm />
-                <SignUpSelfAcc />
+                <div onClick={() => setRegistrationType('personal')}>Личный аккаунт</div>
+                <div onClick={() => setRegistrationType('corp')}>Корпоративный аккаунт</div>
+                {registrationType === 'personal' ? <SignUpSelfAcc /> : <SignUpForm />}
                 <ImagePipe />
             </div>
         </div>

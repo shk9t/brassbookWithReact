df45774f0ff29af5636b39284bff775c389de76b
shk9t <147887719+shk9t@users.noreply.github.com>
2024-02-26 22:12:07 +0300

    modified:   src/components/signUp/SignUp.tsx

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -14,7 +14,7 @@ function SignUp() {
             <div className="sign-in__container container">
                 <div className="sign-in__info">
                     <NavLink to={'/'} className="sign-in__backlink"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M20 12H4M4 12L10 18M4 12L10 6" stroke="#190636" strokeOpacity="0.3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M20 12H4M4 12L10 18M4 12L10 6" stroke="#190636" strokeOpacity="0.3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>Вернуться на главную</NavLink>
                     <h1 className={"sign-in__title"}>Регистрация</h1>
                     <p className="sign-in__text">Выберите тип регистрации и введите необходимые данные. <br/>
